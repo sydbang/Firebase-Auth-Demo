75e898a41871ffbc4304269cad35f18ca1577ab9
sydbang <sydney.bang94@gmail.com>
2022-03-15 09:31:09 +0900

    Initial Commit

---
+++ Firebase Auth Demo/Firebase_Auth_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Auth_DemoApp.swift
+//  Firebase Auth Demo
+//
+//  Created by Sunghee Bang on 2022-03-15.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Auth_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
