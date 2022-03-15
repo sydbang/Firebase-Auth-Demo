868823bde082e793f0a38fff50c966c22cee7f27
sydbang <sydney.bang94@gmail.com>
2022-03-15 09:48:24 +0900

    initialize project
    - connect to firebase
    - pod install and git ignore created

--- Firebase Auth Demo/Firebase_Auth_DemoApp.swift
+++ Firebase Auth Demo/Firebase_Auth_DemoApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Firebase_Auth_DemoApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
